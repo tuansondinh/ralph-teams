d6f1f19e3d43471be2d3af5dfbc1cc5ecc52417e
Tuan Son <tuan.dinh@interzero.de>
2026-03-19 14:20:10 +0100

    feat(us-002): add multiply utility

--- utils.js
+++ utils.js
@@ -2,4 +2,11 @@ function add(a, b) {
   return a + b;
 }
 
-module.exports = add;
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = {
+  add,
+  multiply,
+};
--- utils.test.js
+++ utils.test.js
@@ -1,6 +1,9 @@
 const assert = require('node:assert/strict');
-const add = require('./utils');
+const { add, multiply } = require('./utils');
 
 assert.strictEqual(add(2, 3), 5, 'add should sum positive operands');
 assert.strictEqual(add(-4, -6), -10, 'add should sum negative operands');
 assert.strictEqual(add(0, 0), 0, 'add should sum zero operands');
+assert.strictEqual(multiply(2, 3), 6, 'multiply should multiply positive operands');
+assert.strictEqual(multiply(-4, 5), -20, 'multiply should preserve operand signs');
+assert.strictEqual(multiply(0, 9), 0, 'multiply should return zero when either operand is zero');
