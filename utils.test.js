1844001191def7ab019bd5576f3456d10c090273
Tuan Son <tuan.dinh@interzero.de>
2026-03-19 14:54:29 +0100

    feat(us-002): add multiply utility

--- utils.test.js
+++ utils.test.js
@@ -3,11 +3,20 @@ const utils = require('./utils');
 
 assert.deepStrictEqual(
   Object.keys(utils),
-  ['add'],
-  'utils should only export add for US-001',
+  ['add', 'multiply'],
+  'utils should export add and multiply for US-002',
 );
 
 assert.strictEqual(typeof utils.add, 'function', 'add should be exported as a function');
 assert.strictEqual(utils.add(2, 3), 5, 'add should sum positive operands');
 assert.strictEqual(utils.add(-4, -6), -10, 'add should sum negative operands');
 assert.strictEqual(utils.add(0, 0), 0, 'add should sum zero operands');
+
+assert.strictEqual(
+  typeof utils.multiply,
+  'function',
+  'multiply should be exported as a function',
+);
+assert.strictEqual(utils.multiply(2, 3), 6, 'multiply should multiply positive operands');
+assert.strictEqual(utils.multiply(-4, 5), -20, 'multiply should preserve operand signs');
+assert.strictEqual(utils.multiply(0, 7), 0, 'multiply should return zero when an operand is zero');
