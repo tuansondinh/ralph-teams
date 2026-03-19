78d64b4ea3f0ab41bb9a96d142a23e8323905d96
Tuan Son <tuan.dinh@interzero.de>
2026-03-19 14:19:52 +0100

    feat: add capitalize utility

--- utils.js
+++ utils.js
@@ -2,4 +2,13 @@ function add(a, b) {
   return a + b;
 }
 
+function capitalize(str) {
+  if (str === '') {
+    return '';
+  }
+
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+
 module.exports = add;
+module.exports.capitalize = capitalize;
--- utils.test.js
+++ utils.test.js
@@ -1,6 +1,12 @@
 const assert = require('node:assert/strict');
 const add = require('./utils');
+const { capitalize } = require('./utils');
 
 assert.strictEqual(add(2, 3), 5, 'add should sum positive operands');
 assert.strictEqual(add(-4, -6), -10, 'add should sum negative operands');
 assert.strictEqual(add(0, 0), 0, 'add should sum zero operands');
+
+assert.strictEqual(capitalize('hello world'), 'Hello world', 'capitalize should uppercase the first character of normal strings');
+assert.strictEqual(capitalize(''), '', 'capitalize should return an empty string unchanged');
+assert.strictEqual(capitalize('hello'), 'Hello', 'capitalize should normalize all-lowercase strings');
+assert.strictEqual(capitalize('HELLO'), 'Hello', 'capitalize should normalize all-uppercase strings');
