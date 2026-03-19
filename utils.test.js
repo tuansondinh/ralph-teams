b3825f12a74885e6c866a80278fa657403679a51
Tuan Son <tuan.dinh@interzero.de>
2026-03-19 14:51:19 +0100

    feat: scope root utils to add

--- utils.test.js
+++ utils.test.js
@@ -1,15 +1,13 @@
 const assert = require('node:assert/strict');
-const { add, multiply, capitalize } = require('./utils');
+const utils = require('./utils');
 
-assert.strictEqual(add(2, 3), 5, 'add should sum positive operands');
-assert.strictEqual(add(-4, -6), -10, 'add should sum negative operands');
-assert.strictEqual(add(0, 0), 0, 'add should sum zero operands');
+assert.deepStrictEqual(
+  Object.keys(utils),
+  ['add'],
+  'utils should only export add for US-001',
+);
 
-assert.strictEqual(multiply(2, 3), 6, 'multiply should multiply positive operands');
-assert.strictEqual(multiply(-4, 5), -20, 'multiply should preserve operand signs');
-assert.strictEqual(multiply(0, 9), 0, 'multiply should return zero when either operand is zero');
-
-assert.strictEqual(capitalize('hello world'), 'Hello world', 'capitalize should uppercase the first character of normal strings');
-assert.strictEqual(capitalize(''), '', 'capitalize should return an empty string unchanged');
-assert.strictEqual(capitalize('hello'), 'Hello', 'capitalize should normalize all-lowercase strings');
-assert.strictEqual(capitalize('HELLO'), 'Hello', 'capitalize should normalize all-uppercase strings');
+assert.strictEqual(typeof utils.add, 'function', 'add should be exported as a function');
+assert.strictEqual(utils.add(2, 3), 5, 'add should sum positive operands');
+assert.strictEqual(utils.add(-4, -6), -10, 'add should sum negative operands');
+assert.strictEqual(utils.add(0, 0), 0, 'add should sum zero operands');
