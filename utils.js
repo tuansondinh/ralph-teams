998d48b0f032d8af1ab5fc8386f4806ca4710a22
Tuan Son <tuan.dinh@interzero.de>
2026-03-19 14:17:11 +0100

    feat(us-001): add root add utility

--- utils.js
+++ utils.js
@@ -2,13 +2,4 @@ function add(a, b) {
   return a + b;
 }
 
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-}
-
-function multiply(a, b) {
-  const result = a * b;
-  return result === 0 ? 0 : result;
-}
-
-module.exports = { add, capitalize, multiply };
+module.exports = add;
